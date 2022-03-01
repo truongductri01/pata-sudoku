e612be32d8656fb3f7a624f06ed94f699d15bab2
Abrar Washi <abrarwashi4@gmail.com>
2022-02-28 19:39:34 -0500

    regiter page setup

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import "./signup.css";
+import { Link } from "react-router-dom";
 
-function SignUp() {
-  return <div>SignUp</div>;
+
+export default function Signup() {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirmpassword,confirm]=useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 8 && password==confirmpassword;
+  }
+
+ 
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      
+    }
+  return(
+    <div className="signup">
+      <h1 className="sudoku">Register</h1>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <label style={{fontSize:"18px"}}>Email
+            <input className="username"
+                type="email" 
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+                  />
+                </label>
+                <br></br>
+                <br></br>
+          <label style={{fontSize:"18px"}}>password
+            <br></br>
+            <input className="pass"
+              type="password" 
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+              />
+              </label>
+              <br></br>
+              <br></br>
+          <label style={{fontSize:"18px"}}>Confirm password
+            <br></br>
+            <input className="confirmpass"
+              type="password" 
+              value={confirmpassword}
+              onChange={(e) => confirm(e.target.value)}
+              />
+              </label>
+              <br></br>
+              <br></br>
+              <button className="register" disabled={!validateForm()}>register</button>
+            </form> 
+            <br></br>
+            <Link to={"/login"}><button>login</button></Link>
+        </div> 
+
+
+    </div>
+  )
 }
-
-export default SignUp;
+  
