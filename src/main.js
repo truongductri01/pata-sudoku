60c5165768e77b01b810cb83d8254da8f0ad01f0
Abrar Washi <abrarwashi4@gmail.com>
2022-03-09 11:11:54 -0500

    backend-connect

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./main.css"
+import { useNavigate } from "react-router-dom";
 
 export default function Main(){
     const[roomid, setroomid]=useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        navigate("/");
+
+
         
       }
     return(
